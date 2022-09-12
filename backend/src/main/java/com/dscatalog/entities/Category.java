f063461552ea1f8f6b592ed8910cefa03b2b0826
bruno lessa <brunolessa@live.com>
2022-09-12 12:02:50 -0300

    category resource

---
+++ backend/src/main/java/com/dscatalog/entities/Category.java
@@ -0,0 +1,52 @@
+package com.dscatalog.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+    @Id
+    private Long id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return id.equals(category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ backend/src/main/java/com/dscatalog/resources/CategoryResources.java
@@ -0,0 +1,23 @@
+package com.dscatalog.resources;
+
+import com.dscatalog.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResources {
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1L, "Books"));
+        list.add(new Category(2L, "Electronics"));
+        return ResponseEntity.ok().body(list);
+    }
+}
