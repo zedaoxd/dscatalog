11d834d0b7e3fb27bf4e8b5726cb042fe44f5c5b
bruno lessa <brunolessa@live.com>
2022-09-12 12:39:14 -0300

    database seeding

--- backend/src/main/java/com/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.dscatalog.entities.Category;
 import com.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,7 +14,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
-
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return repository.findAll();
     }
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into tb_category(name) values ('Books');
+insert into tb_category(name) values ('Electronics');
+insert into tb_category(name) values ('Computers');
