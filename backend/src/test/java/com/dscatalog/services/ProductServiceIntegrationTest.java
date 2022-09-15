7cd81c855839d876545de6d025628918b7cf9a5c
bruno lessa <brunolessa@live.com>
2022-09-14 23:34:59 -0300

    integration test findAllPage

--- backend/src/test/java/com/dscatalog/services/ProductServiceIntegrationTest.java
+++ backend/src/test/java/com/dscatalog/services/ProductServiceIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.dscatalog.services;
 
+import com.dscatalog.dto.ProductDTO;
 import com.dscatalog.repositories.ProductRepository;
 import com.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
@@ -7,13 +8,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIntegrationTest {
 
     @Autowired
     private ProductService service;
-
     @Autowired
     private ProductRepository repository;
 
@@ -26,6 +31,36 @@ public class ProductServiceIntegrationTest {
         existingId = 1L;
         nonExistingId = -1L;
         countTotalProducts = 25L;
+    }
+
+    @Test
+    public void findAllPageShouldReturnSortedPageWhenSortByName() {
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+        Page<ProductDTO> result = service.findAllPage(pageRequest);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+        Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+        Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
+    }
+
+    @Test
+    public void findAllPageShouldReturnEmptyPageWhenPageDoesNotExists() {
+        PageRequest pageRequest = PageRequest.of(50, 10);
+        Page<ProductDTO> result = service.findAllPage(pageRequest);
+
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void findAllPageShouldReturnPageWhenPage0Size10() {
+        PageRequest pageRequest = PageRequest.of(0, 10);
+        Page<ProductDTO> result = service.findAllPage(pageRequest);
+
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(0, result.getNumber()); // número da página
+        Assertions.assertEquals(10, result.getSize()); // Tamanho da página
+        Assertions.assertEquals(countTotalProducts, result.getTotalElements());
     }
 
     @Test
