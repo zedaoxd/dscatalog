8b6458a9514a64051277698d7fba2659407256db
bruno lessa <brunolessa@live.com>
2022-09-15 23:20:25 -0300

    custom validation response

--- backend/src/main/java/com/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import com.dscatalog.services.exceptions.DatabaseException;
 import com.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -34,6 +35,23 @@ public class ResourceExceptionHandler {
         err.setError("can't delete this entity, foreign key referenced");
         err.setMessage(e.getMessage());
         err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validationException(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Validation exception");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+
+        e.getBindingResult().getFieldErrors().forEach(f -> {
+            err.addError(f.getField(), f.getDefaultMessage());
+        });
+
         return ResponseEntity.status(status).body(err);
     }
 }
