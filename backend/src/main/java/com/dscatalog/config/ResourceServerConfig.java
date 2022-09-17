156e860a438caeab8c5164b9bf44f671400428b6
bruno lessa <brunolessa@live.com>
2022-09-17 14:11:16 -0300

    H2 config

--- backend/src/main/java/com/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/dscatalog/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,14 +10,18 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
+    private Environment environment;
+    @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] PUBLIC = { "/oauth/token" };
+    private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
     private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
     private static final String[] ADMIN = { "/users/**" };
 
@@ -27,6 +32,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        // config H2
+        if (Arrays.asList(environment.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 .antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
