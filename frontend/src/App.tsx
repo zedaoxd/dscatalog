bfb5ce7aceca0e10846aedb84fd9e73f4a7e674e
bruno lessa <brunolessa@live.com>
2022-09-20 23:12:34 -0300

    import fonts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
-function App() {
+const App = () => {
   return (
-    <h1>Hello DSCatalog</h1>
+    <>
+      <Navbar />
+      <h1>Hello DSCatalog</h1>
+    </>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,24 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-primary">
+      <div>
+        <a href="link">
+          <h4>DS Catalog</h4>
+        </a>
+        <div>
+          <ul>
+            <li>
+              <a href="link">HOME</a>
+            </li>
+            <li>
+              <a href="link">CATÁLOGO</a>
+            </li>
+            <li>
+              <a href="link">ADMIN</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
