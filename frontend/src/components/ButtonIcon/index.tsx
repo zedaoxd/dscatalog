746eada0623e380865918767083e67459c584c1e
bruno lessa <brunolessa@live.com>
2022-09-21 14:17:35 -0300

    finish layout x-Small

---
+++ frontend/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const ButtonIcon = () => {
+  return (
+    <button className="btn btn-primary btn-icon">
+      <h6>INICIE AGORA SUA BUSCA</h6>
+    </button>
+  );
+};
+
+export default ButtonIcon;
