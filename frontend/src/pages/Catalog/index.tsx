5e1fe55ba834640979ed2dce2f752d9fb35140c7
bruno lessa <brunolessa@live.com>
2022-09-22 09:49:13 -0300

    grid error

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -6,7 +6,23 @@ const Catalog = () => {
     <>
       <Navbar />
       <div className="container my-4">
-        <ProductCard />
+        <div className="row">
+          <div className="col-sm-6">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6">
+            <ProductCard />
+          </div>
+          <div className="col-sm-6">
+            <ProductCard />
+          </div>
+        </div>
       </div>
     </>
   );
