6a5631a1c7a6efaacfab374195c631edcdc6aa86
Bruno Lessa <brunolessa@live.com>
2022-10-13 14:50:58 -0300

    props

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
           </div>
           <div>
             <Link to="/products">
-              <ButtonIcon />
+              <ButtonIcon text="Inicie agora sua busca"/>
             </Link>
           </div>
         </div>
