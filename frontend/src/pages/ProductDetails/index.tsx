5ef269f2c6e892098e33be2bec01d119b040bdec
Bruno Lessa <brunolessa@live.com>
2022-10-14 00:29:12 -0300

    Product Details layout mobile

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,12 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
 
+import './styles.css';
+
 const ProductDetails = () => {
     return (
         <div className="product-details-container">
-            <div className="product-details-card">
+            <div className="base-card product-details-card">
                 <div className="goback-container">
                     <ArrowIcon />
                     <h2>VOLTAR</h2>
