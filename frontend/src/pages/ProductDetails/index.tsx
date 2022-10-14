5e6634ae3c0a38622a603fb99ba8a37bfa79dfed
Bruno Lessa <brunolessa@live.com>
2022-10-14 00:04:57 -0300

    Product Details HTML

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,5 +1,34 @@
+import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+import ProductPrice from 'components/ProductPrice';
+
 const ProductDetails = () => {
-    return <h1>ProductDetails</h1>;
+    return (
+        <div className="product-details-container">
+            <div className="product-details-card">
+                <div className="goback-container">
+                    <ArrowIcon />
+                    <h2>VOLTAR</h2>
+                </div>
+                <div className="row">
+                    <div className="col-xl-6">
+                        <div className="img-container">
+                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="nome do produto" />
+                        </div>
+                        <div className="name-price-container">
+                            <h1>Nome do produto</h1>
+                            <ProductPrice price={2345.67} />
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <div className="description-container">
+                            <h2>Descrição do produto</h2>
+                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste quia perferendis voluptate, est dolore vero reprehenderit blanditiis a at doloremque quas cupiditate iure iusto natus pariatur voluptatem! Odit, libero exercitationem?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ProductDetails;
