46ff212bb5c7f99f3d0703db35a24790625afd98
Bruno Lessa <brunolessa@live.com>
2022-10-14 12:31:56 -0300

    Axios, useState and useEffect

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,10 +1,23 @@
 import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
 import ProductPrice from 'components/ProductPrice';
 import { Link } from 'react-router-dom';
+import { Product } from 'types/product';
+import axios from 'axios';
 
 import './styles.css';
+import { BASE_URL } from 'utils/requests';
+import { useEffect, useState } from 'react';
 
 const ProductDetails = () => {
+
+    const [product, setProduct] = useState<Product>();
+
+    useEffect(() => {
+        axios.get(BASE_URL + "/products/1")
+            .then(response => setProduct(response.data));
+    }, []);
+    
+
     return (
         <div className="product-details-container">
             <div className="base-card product-details-card">
@@ -17,17 +30,17 @@ const ProductDetails = () => {
                 <div className="row">
                     <div className="col-xl-6">
                         <div className="img-container">
-                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg" alt="nome do produto" />
+                            <img src={product?.imgUrl} alt={product?.name} />
                         </div>
                         <div className="name-price-container">
-                            <h1>Nome do produto</h1>
-                            <ProductPrice price={2345.67} />
+                            <h1>{product?.name}</h1>
+                            { product && <ProductPrice price={product?.price} />}
                         </div>
                     </div>
                     <div className="col-xl-6">
                         <div className="description-container">
                             <h2>Descrição do produto</h2>
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste quia perferendis voluptate, est dolore vero reprehenderit blanditiis a at doloremque quas cupiditate iure iusto natus pariatur voluptatem! Odit, libero exercitationem?</p>
+                            <p>{product?.description}</p>
                         </div>
                     </div>
                 </div>
