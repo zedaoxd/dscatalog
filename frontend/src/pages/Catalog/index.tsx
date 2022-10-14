eb7ca1cf85c296f53d48adce32b669e85e7e449d
Bruno Lessa <brunolessa@live.com>
2022-10-14 10:25:47 -0300

    Catalog-ProductDetails links

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,4 +1,5 @@
 import ProductCard from 'components/ProductCard';
+import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
 
 const Catalog = () => {
@@ -26,19 +27,29 @@ const Catalog = () => {
     <div className="container my-4">
       <div className="row">
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product}/>
+          <Link to="/products/1">
+            <ProductCard product={product}/>
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product}/>
+          <Link to="/products/1">
+            <ProductCard product={product}/>
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product}/>
+          <Link to="/products/1">
+            <ProductCard product={product}/>
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product}/>
+          <Link to="/products/1">
+            <ProductCard product={product}/>
+          </Link>
         </div>
         <div className="col-sm-6 col-lg-4 col-xl-3">
-          <ProductCard product={product}/>
+          <Link to="/products/1">
+            <ProductCard product={product}/>
+          </Link>
         </div>
       </div>
     </div>
