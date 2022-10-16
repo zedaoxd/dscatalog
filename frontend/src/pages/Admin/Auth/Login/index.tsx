32a28494b905c36490d4bf4fdb2f31ad5a49a730
Bruno Lessa <brunolessa@live.com>
2022-10-16 15:06:02 -0300

    conditional styling

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -42,7 +42,7 @@ const Login = () => {
                             }
                         })}
                         type="text" 
-                        className="form-control base-input" 
+                        className={`form-control base-input ${errors.username && 'is-invalid'}`}
                         placeholder="Email" 
                         name="username"
                     />
@@ -54,7 +54,7 @@ const Login = () => {
                             required: "Campo obrigatório"
                         })}
                         type="password" 
-                        className="form-control base-input" 
+                        className={`form-control base-input ${errors.password && 'is-invalid'}`}
                         placeholder="Password" 
                         name="password" 
                     />
