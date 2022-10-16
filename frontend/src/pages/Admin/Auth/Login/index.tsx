d6b6122e4f477fcc11bb9fce9577833749c88c9f
Bruno Lessa <brunolessa@live.com>
2022-10-16 15:01:02 -0300

    form validation

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -14,7 +14,7 @@ type FormData = {
 const Login = () => {
 
     const [hasError, setHasError] = useState<boolean>(false);
-    const { register, handleSubmit } = useForm<FormData>();
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
     const onSubmit = (formData: FormData) => {
         requestBackendLogin(formData)
@@ -34,21 +34,31 @@ const Login = () => {
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-4">
                     <input 
-                        {...register("username")}
+                        {...register("username", {
+                            required: "Campo obrigatório",
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message: "Email inválido"
+                            }
+                        })}
                         type="text" 
                         className="form-control base-input" 
                         placeholder="Email" 
                         name="username"
                     />
+                    <div className="invalid-feedback d-block">{errors.username?.message}</div>
                 </div>
                 <div className="mb-2">
                     <input 
-                        {...register("password")}
+                        {...register("password", {
+                            required: "Campo obrigatório"
+                        })}
                         type="password" 
                         className="form-control base-input" 
                         placeholder="Password" 
                         name="password" 
                     />
+                    <div className="invalid-feedback d-block">{errors.password?.message}</div>
                 </div>
                 <Link to="/admin/auth/recover" className="login-link-recover">
                     Esqueci a senha
