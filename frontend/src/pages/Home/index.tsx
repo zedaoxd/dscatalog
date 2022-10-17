14e1fe3ca115568ce0e6447a42ead89ac1ee0e5b
Bruno Lessa <brunolessa@live.com>
2022-10-17 15:22:04 -0300

    check user authenticated

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -2,10 +2,12 @@ import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
 import ButtonIcon from 'components/ButtonIcon';
 import './styles.css';
 import { Link } from 'react-router-dom';
+import { isAuthenticated } from 'utils/requests';
 
 export default function Home() {
   return (
     <div className="home-container">
+      <h1>{isAuthenticated() ? 'autenticado' : 'não autenticado'}</h1>
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,6 +1,15 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import qs from 'qs';
 import history from './history';
+import jwtDecode from 'jwt-decode';
+
+type Role = "ROLE_OPERATOR" | "ROLE_ADMIN";
+
+type TokenData = {
+    exp: number;
+    user_name: string;
+    authorities: Role[];
+};
 
 type LoginResponse = {
     access_token: string;
@@ -91,3 +100,17 @@ axios.interceptors.response.use((response) => {
     
     return Promise.reject(error);
 });
+
+export const getTokenData = () : TokenData | undefined => {
+    try {
+        return jwtDecode(getAuthData().access_token) as TokenData;
+    } catch (error) {
+        return undefined;
+    }
+}
+
+export const isAuthenticated = () : boolean => {
+    const tokenData = getTokenData();
+
+    return (tokenData && tokenData?.exp * 1_000 > Date.now()) ? true : false;
+}
