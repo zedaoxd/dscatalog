f939fdc944098a8b8e25425749093f56c9e26d14
bruno lessa <brunolessa@live.com>
2022-10-26 14:41:03 -0300

    Produ crud card horizontal responsivity

--- frontend/src/pages/Admin/Products/ProductCrudCard/index.tsx
+++ frontend/src/pages/Admin/Products/ProductCrudCard/index.tsx
@@ -14,7 +14,7 @@ const ProductCrudCard = ({ product }: Props) => {
       <div className="product-crud-card-top-container">
         <img src={product.imgUrl} alt={product.name} />
       </div>
-      <div>
+      <div className="product-crud-card-description">
         <div className="product-crud-card-bottom-container">
           <h6>{product.name}</h6>
           <ProductPrice price={product.price} />
