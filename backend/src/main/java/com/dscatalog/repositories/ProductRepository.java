ad240c5ebee9b9be2e36d9a634e70bcdcab97ee8
bruno lessa <brunolessa@live.com>
2022-10-28 19:38:07 -0300

    Product list from backend

--- backend/src/main/java/com/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/dscatalog/repositories/ProductRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,8 +19,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             // need coalesce COALESCE(:categories, NULL) to work on postgres but with it gives error on h2
             "WHERE (:categories IS NULL OR cats IN :categories) " +
             "AND (UPPER(prod.name) LIKE UPPER(CONCAT('%', :name, '%')))")
-    Page<Product> find(List<Category> categories, String name, Pageable pageable);
+    Page<Product> find(@Param("categories") List<Category> categories,
+                       @Param("name") String name,
+                       Pageable pageable);
 
     @Query(value = "SELECT prod FROM Product prod JOIN FETCH prod.categories WHERE prod IN :products")
-    List<Product> findProductWithCategories(List<Product> products);
+    List<Product> findProductWithCategories(@Param("products") List<Product> products);
 }
--- frontend/src/pages/Admin/Products/List/index.tsx
+++ frontend/src/pages/Admin/Products/List/index.tsx
@@ -1,32 +1,30 @@
+import { AxiosRequestConfig } from 'axios';
 import ProductCrudCard from 'pages/Admin/Products/ProductCrudCard';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Product } from 'types/product';
+import { SpringPage } from 'types/vendor/spring';
+import { requestBackend } from 'utils/requests';
 import './styles.css';
 
 export const List = () => {
-  const product = {
-    id: 2,
-    name: 'Smart TV',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    price: 2190.0,
-    imgUrl:
-      'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/2-big.jpg',
-    date: '2020-07-14T10:00:00Z',
-    categories: [
-      {
-        id: 1,
-        name: 'Livros',
+  const [page, setPage] = useState<SpringPage<Product>>();
+
+  useEffect(() => {
+    const config: AxiosRequestConfig = {
+      method: 'GET',
+      url: '/products',
+      params: {
+        page: 0,
+        size: 50,
+        sort: 'name,desc',
       },
-      {
-        id: 2,
-        name: 'Eletrônicos',
-      },
-      {
-        id: 3,
-        name: 'Computadores',
-      },
-    ],
-  };
+    };
+
+    requestBackend(config).then((response) => {
+      setPage(response.data);
+    });
+  }, []);
 
   return (
     <div className="product-crud-container">
@@ -40,16 +38,13 @@ export const List = () => {
         <div className="base-card product-filter-container">Search bar</div>
       </div>
       <div className="row">
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
-        <div className="col-sm-6 col-md-12">
-          <ProductCrudCard product={product} />
-        </div>
+        {page?.content.map((p) => (
+          <div key={p.id} className="col-sm-6 col-md-12">
+            <ProductCrudCard product={p} />
+          </div>
+        ))}
       </div>
+      ;
     </div>
   );
 };
