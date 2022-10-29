8c816278383e2992ea789693740edf4bed43b28f
bruno lessa <brunolessa@live.com>
2022-10-28 21:24:40 -0300

    Product save

--- frontend/src/pages/Admin/Auth/Login/index.tsx
+++ frontend/src/pages/Admin/Auth/Login/index.tsx
@@ -8,94 +8,103 @@ import { requestBackendLogin } from 'utils/requests';
 import { saveAuthData } from 'utils/storage';
 import './styles.css';
 
-
-type FormData = {
-    username: string;
-    password: string;
-}
+type CredentialsDTO = {
+  username: string;
+  password: string;
+};
 
 type LocationState = {
-    from: string;
+  from: string;
 };
 
 const Login = () => {
+  const location = useLocation<LocationState>();
+  const { from } = location.state || { from: { pathname: '/admin' } };
 
-    const location = useLocation<LocationState>();
-    const { from } = location.state || {from: {pathname: '/admin'}};
+  const { setAuthContextData } = useContext(AuthContext);
+  const [hasError, setHasError] = useState<boolean>(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CredentialsDTO>();
 
-    const { setAuthContextData } = useContext(AuthContext);
-    const [hasError, setHasError] = useState<boolean>(false);
-    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const history = useHistory();
 
-    const history = useHistory();
+  const onSubmit = (formData: CredentialsDTO) => {
+    requestBackendLogin(formData)
+      .then((response) => {
+        saveAuthData(response.data);
+        setHasError(false);
+        setAuthContextData({
+          authenticated: true,
+          tokenData: getTokenData(),
+        });
+        history.replace(from);
+      })
+      .catch((error) => setHasError(true));
+  };
 
-    const onSubmit = (formData: FormData) => {
-        requestBackendLogin(formData)
-            .then(response => {
-                saveAuthData(response.data);
-                setHasError(false);
-                setAuthContextData({
-                    authenticated: true,
-                    tokenData: getTokenData(),
-                });
-                history.replace(from);
-            })
-            .catch(error => setHasError(true));
-    };
-
-    return (
-        <div className="base-card login-card">
-            <h1>LOGIN</h1>
-            {
-                hasError &&
-                <div className="alert alert-danger">
-                    Erro ao tentar efetuar o login!
-                </div>
-            }
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="mb-4">
-                    <input 
-                        {...register("username", {
-                            required: "Campo obrigatório",
-                            pattern: {
-                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                message: "Email inválido"
-                            }
-                        })}
-                        type="text" 
-                        className={`form-control base-input ${errors.username && 'is-invalid'}`}
-                        placeholder="Email" 
-                        name="username"
-                    />
-                    <div className="invalid-feedback d-block">{errors.username?.message}</div>
-                </div>
-                <div className="mb-2">
-                    <input 
-                        {...register("password", {
-                            required: "Campo obrigatório"
-                        })}
-                        type="password" 
-                        className={`form-control base-input ${errors.password && 'is-invalid'}`}
-                        placeholder="Password" 
-                        name="password" 
-                    />
-                    <div className="invalid-feedback d-block">{errors.password?.message}</div>
-                </div>
-                <Link to="/admin/auth/recover" className="login-link-recover">
-                    Esqueci a senha
-                </Link>
-                <div className="login-submit">
-                    <ButtonIcon text="Fazer login" />
-                </div>
-                <div className="signup-container">
-                    <span className="not-registered">Não tem Cadastro?</span>
-                    <Link to="/admin/auth/register" className="login-link-register">
-                        CADASTRAR
-                    </Link>
-                </div>
-            </form>
+  return (
+    <div className="base-card login-card">
+      <h1>LOGIN</h1>
+      {hasError && (
+        <div className="alert alert-danger">
+          Erro ao tentar efetuar o login!
         </div>
-    );
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+          <input
+            {...register('username', {
+              required: 'Campo obrigatório',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'Email inválido',
+              },
+            })}
+            type="text"
+            className={`form-control base-input ${
+              errors.username && 'is-invalid'
+            }`}
+            placeholder="Email"
+            name="username"
+          />
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
+          </div>
+        </div>
+        <div className="mb-2">
+          <input
+            {...register('password', {
+              required: 'Campo obrigatório',
+            })}
+            type="password"
+            className={`form-control base-input ${
+              errors.password && 'is-invalid'
+            }`}
+            placeholder="Password"
+            name="password"
+          />
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
+          </div>
+        </div>
+        <Link to="/admin/auth/recover" className="login-link-recover">
+          Esqueci a senha
+        </Link>
+        <div className="login-submit">
+          <ButtonIcon text="Fazer login" />
+        </div>
+        <div className="signup-container">
+          <span className="not-registered">Não tem Cadastro?</span>
+          <Link to="/admin/auth/register" className="login-link-register">
+            CADASTRAR
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,36 +1,109 @@
+import { AxiosRequestConfig } from 'axios';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
+import { Product } from 'types/product';
+import { requestBackend } from 'utils/requests';
 import './styles.css';
 
 export const Form = () => {
+  const history = useHistory();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Product>();
+
+  const onSubmit = (formData: Product) => {
+    const config: AxiosRequestConfig = {
+      method: 'POST',
+      url: '/products',
+      data: {
+        ...formData,
+        categories: [{ id: 1, name: '' }],
+        imgUrl:
+          'https://www.amd.com/system/files/2022-09/1536834-amd-ryzen-7-7000-series-PIB-angle-1260x709.png',
+      },
+      withCredentials: true,
+    };
+
+    requestBackend(config).then((response) => console.log(response.data));
+  };
+
+  const handleCancel = () => {
+    history.push('/admin/products');
+  };
+
   return (
     <div className="product-crud-container">
       <div className="base-card product-crud-form-card">
         <h1 className="product-crud-form-title">DADOS DO PRODUTO</h1>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="row product-crud-inputs-container">
             <div className="col-lg-6 product-crud-inputs-left-container">
               <div className="margin-bottom-30">
-                <input type="text" className="form-control base-input" />
+                <input
+                  {...register('name', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.name && 'is-invalid'
+                  }`}
+                  placeholder="Nome do Produto"
+                  name="name"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.name?.message}
+                </div>
               </div>
+
               <div className="margin-bottom-30">
-                <input type="text" className="form-control base-input" />
+                <input
+                  {...register('price', {
+                    required: 'Campo obrigatório',
+                  })}
+                  type="text"
+                  className={`form-control base-input ${
+                    errors.price && 'is-invalid'
+                  }`}
+                  placeholder="Preço"
+                  name="price"
+                />
+                <div className="invalid-feedback d-block">
+                  {errors.price?.message}
+                </div>
               </div>
-              <div>
+
+              {/* <div>
                 <input type="text" className="form-control base-input" />
-              </div>
+              </div> */}
             </div>
             <div className="col-lg-6">
               <div>
                 <textarea
-                  name=""
+                  {...register('description', {
+                    required: 'Campo obrigatório',
+                  })}
+                  className={`form-control base-input h-auto ${
+                    errors.description && 'is-invalid'
+                  }`}
+                  placeholder="Descrição"
+                  name="description"
                   rows={10}
-                  className="form-control base-input h-auto"
                 />
+                <div className="invalid-feedback d-block">
+                  {errors.description?.message}
+                </div>
               </div>
             </div>
           </div>
 
           <div className="product-crud-buttons-container">
-            <button className="btn btn-outline-danger product-crud-button">
+            <button
+              className="btn btn-outline-danger product-crud-button"
+              onClick={handleCancel}
+            >
               CANCELAR
             </button>
             <button className="btn btn-primary product-crud-button text-white">
