994645c4aa0c788f79f5a1e407eebd31b3f95910
Bruno Lessa <brunolessa@live.com>
2022-10-31 13:17:23 -0300

    Testing another hasAnyRoles scenario

--- frontend/src/utils/__tests__/auth.spec.ts
+++ frontend/src/utils/__tests__/auth.spec.ts
@@ -19,4 +19,16 @@ describe('hasAnyRoles tests', () => {
         const result = hasAnyRoles(['ROLE_ADMIN']);
         expect(result).toEqual(true);
     });
+
+    test('should return false when user does not have given role', () => {
+
+        jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
+            exp:0, 
+            user_name: '', 
+            authorities: ['ROLE_OPERATOR']
+        });
+
+        const result = hasAnyRoles(['ROLE_ADMIN']);
+        expect(result).toEqual(false);
+    });
 });
