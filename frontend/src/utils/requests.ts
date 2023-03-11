ae3bb44c92e9492d9ce6f2f40ab6750cfc249e01
Bruno Lessa <brunolessa@live.com>
2023-03-11 02:34:16 -0300

    test end point

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -3,8 +3,8 @@ import qs from 'qs';
 import history from './history';
 import { getAuthData } from './storage';
 
-export const BASE_URL =
-  process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+export const BASE_URL = 'https://dscatalog-production-1c02.up.railway.app';
+//process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
